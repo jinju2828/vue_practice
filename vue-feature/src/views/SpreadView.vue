f6fe298af3a4093e84580382d46a6f487a1b17df
jhlee <stir084@gmail.com>
2023-07-13 14:59:57 +0900

    feature: vuex, spread

---
+++ vue-feature/src/views/SpreadView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+      person : {
+        name: 'John',
+        age: 30
+      }
+    }
+
+
+  },
+  mounted() {
+    const extendedPerson = {
+      ...this.person,
+      occupation: 'Developer',
+    };
+    alert(JSON.stringify(extendedPerson));
+  }
+}
+
+</script>
