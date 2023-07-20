abe7b8daf8250dfd06d618e2f7aa0298886a4809
junho <wnsgh1357@gmail.com>
2023-07-20 23:19:17 +0900

    async에 제네릭 넣으면 ESLint 에러 발생 케이스 + 맥에서 작업하던거 에러나는거 설정 파일 수정을 통해 해결

--- vue-feature-typescript/src/views/AsyncView2.vue
+++ vue-feature-typescript/src/views/AsyncView2.vue
@@ -11,10 +11,17 @@ const props = defineProps({
 /* data */
 /* eslint-disable */
 const test = {
-  get: async <T>(
+  //ESLint인 경우 객체에 화살표 함수와 람다를 같이 쓰는 경우 ESLint 에러가 발생한다. 화살표 함수가 아닌 일반 함수로 만들어야한다.
+  //그래서 lintOnSave를 해야하는 경우가 좀 있다.
+  /*get: async <T>(
       baseUrl: string,
       pathVariable?: string,
   ) => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    return response.data;
+  },*/
+  //ESLint를 통과하려면 아래와 같이 사용한다.
+  async get<T>(baseUrl: string, pathVariable?: string) {
     const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
     return response.data;
   },
